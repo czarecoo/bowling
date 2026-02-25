be441fdb8e6f6000612f826f38bd037785781b11
Cezary Witkowski <czarecoo@gmail.com>
2026-02-25 17:51:51 +0100

    Add classic game implementation, gutter game test and junit dependency

---
+++ src/main/java/org/czareg/ClassicGame.java
@@ -0,0 +1,13 @@
+package org.czareg;
+
+class ClassicGame implements Game {
+
+    @Override
+    public void roll(int pins) {
+    }
+
+    @Override
+    public int score() {
+        return 0;
+    }
+}
---
+++ src/test/java/org/czareg/ClassicGameTests.java
@@ -0,0 +1,24 @@
+package org.czareg;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ClassicGameTests {
+
+    Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new ClassicGame();
+    }
+
+    @Test
+    void testGutterGame() {
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+}
